38a9ab09c39496f20669d3e334af6a5ffa85fabf
Thomas Michelet <thomas.michelet@wifirst.fr>
2024-06-21 07:59:15 +0200

    Zoom control

--- src/pages/LaundryPublic.jsx
+++ src/pages/LaundryPublic.jsx
@@ -3,12 +3,82 @@ import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch';
 import { Typography, Stack } from '@mui/material'
 import HeaderCard, { Card } from "../components/HeaderCard";
-import {APIProvider, Map, AdvancedMarker, Pin, useMap} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, AdvancedMarker, Pin, useMap } from '@vis.gl/react-google-maps';
+import { createRoot } from 'react-dom/client'
+import {Button, Box, useTheme} from '@mui/material'
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import {MarkerClusterer} from '@googlemaps/markerclusterer';
 import landries from '../algolia.json'
-
+//const landries = [_landries[0], _landries[1]]
 const searchClient = algoliasearch(import.meta.env.VITE_ALGOLIA_APPLICATION_ID, import.meta.env.VITE_ALGOLIA_APPLICATION_KEY);
 
+function ZoomButton({onClick, children}) {
+  const theme = useTheme()
+  return (
+    <Stack
+      onClick={onClick}
+      color={'success'}
+      size={'small'}
+      sx={{
+        cursor: 'pointer',
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+        backgroundColor: 'white',
+        color: theme.palette.success.main,
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: `1px solid ${theme.palette.success.main}`
+      }}
+    >
+      {children}
+    </Stack>
+  )
+}
+function ZoomControl({map}) {
+
+  return (
+    <Stack gap={2} mr={1} mb={1}>
+      <ZoomButton
+        onClick={() => map.setZoom(map.getZoom() + 1)}
+      >
+        <ZoomInIcon />
+      </ZoomButton>
+      <ZoomButton onClick={() => map.setZoom(map.getZoom() - 1)}>
+        <ZoomOutIcon />
+      </ZoomButton>
+    </Stack>
+  )
+}
+function createCenterControl(map) {
+  const controlButton = document.createElement("div");
+  const zoomRoot = createRoot(controlButton)
+  zoomRoot.render(<ZoomControl map={map}/>)
+
+
+  return controlButton;
+}
+
+const initializeControls = (map) => {
+  // Create a DIV to attach the control UI to the Map.
+  const centerControlDiv = document.createElement("div");
+
+// Create the control. This code calls a function that
+// creates a new instance of a button control.
+  const centerControl = createCenterControl(map);
+
+// Append the control to the DIV.
+  centerControlDiv.appendChild(centerControl);
+
+// Add the control to the map at a designated control position
+// by pushing it on the position's array. This code will
+// implicitly add the control to the DOM, through the Map
+// object. You should not attach the control manually.
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].clear()
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv);
+
+}
 const ProxyMap = () => {
   const map = useMap()
   const [markers, setMarkers] = useState([])
@@ -19,6 +89,14 @@ const ProxyMap = () => {
     if (!clusterer.current) {
       clusterer.current = new MarkerClusterer({map});
     }
+    map.setZoom(8)
+    //map.setCenter(landries[0].geometry)
+    const bounds = new google.maps.LatLngBounds();
+    landries.forEach(({geometry}) => bounds.extend(geometry))
+    map.fitBounds(bounds)
+
+    //map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].clear()
+    initializeControls(map)
   }, [map]);
 
   useEffect(() => {
@@ -48,22 +126,23 @@ const ProxyMap = () => {
         title={address}
         ref={marker => setMarkerRef(marker, i)}
       >
-        <Pin background={'#A5D6A7'} glyphColor={'#3B82F6'} borderColor={'#3B82F6'} />
+        <Pin background={'#3B82F6'} glyphColor={'#A5D6A7'} borderColor={'#3B82F6'} />
       </AdvancedMarker>
     ))
 }
 
 function MyMap () {
   return (
-    <Stack sx={{width: 600, height: 400}}>
+    <Stack sx={{width: '100%', height: 400}}>
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_KEY} onLoad={() => console.log('Maps API has loaded.')}>
         <Map
           mapId={'LAUNDRIES'}
-          zoomControl
-          scaleControl
+          zoomControl={false}
+          scaleControl={false}
           mapTypeControl={false}
           streetViewControl={false}
           rotateControl={false}
+          fullscreenControl={false}
         >
           <ProxyMap />
         </Map>
